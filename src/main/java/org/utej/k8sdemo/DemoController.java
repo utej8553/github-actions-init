b7e42625736d1ae213e8c9ee2c290a2d9ff9822a
Utej <91786744+utej8553@users.noreply.github.com>
2026-03-06 14:01:11 +0530

    Update DemoController.java

--- src/main/java/org/utej/k8sdemo/DemoController.java
+++ src/main/java/org/utej/k8sdemo/DemoController.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
     @GetMapping("/home")
     public String home() {
-        return "You just visited /home";
+        return "You just visited 6767";
     }
     @GetMapping("/end")
     public String end() {
-        return "You just visited /end";
+        return "You just visited 6969";
     }
 }
+
 
 
 
